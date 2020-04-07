d946c86bdcf72c27001f3e29d9407d1666ce0e5d
Dylan Walsh <63213378+dyl2u20@users.noreply.github.com>
2020-04-07 02:00:45 +0100

    Create dataTypes.cs
    Some users might be struggling to print a floating point with 1 decimal place, this is because .NET doesn't know what you want to do with your string, thus you must specify this. If you use the .ToString() method, using "F1" (.ToString("F1")) will specify one decimal place. 
    
    Refer to the following link on numeric format strings; https://docs.microsoft.com/en-us/dotnet/standard/base-types/standard-numeric-format-strings

---
+++ dataTypes.cs
@@ -0,0 +1,28 @@
+```csharp
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+class Solution 
+{
+    static void Main(String[] args) 
+    {
+        // Predefined variables from HackerRank
+        int i = 4;
+        double d = 4.0;
+        string s = "HackerRank ";
+    
+        // Declare second integer, double, and String variables.
+        int j;
+        double e;
+        string t;
+        
+        // Read and save an integer, double, and String to your variables.
+        j = Convert.ToInt32(Console.ReadLine());
+        e = Convert.ToDouble(Console.ReadLine());
+        t = Console.ReadLine();
+
+        // Print the sum of both variables on new lines
+        Console.WriteLine($"{i + j}\n{(d+e).ToString("F1")}\n{s+t}");
+    }
+}
