c6551b9ea73ff71bc06e0aa119e89bfb9574efd4
Dylan Walsh <63213378+dyl2u20@users.noreply.github.com>
2020-04-08 01:15:12 +0100

    Add files via upload

---
+++ helloWorld.cs
@@ -0,0 +1,15 @@
+```csharp
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+class Solution
+ {
+    static void Main(String[] args) 
+    {
+        // Assigning ReadLine to string declaration and printing
+        String input = Console.ReadLine(); 
+        Console.WriteLine($"Hello, World.\n{input}");
+    }
+}
+```
