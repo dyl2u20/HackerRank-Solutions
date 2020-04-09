0e38f7a4e894fd158ae4e4929d86f57d26988564
Dylan Walsh <63213378+dyl2u20@users.noreply.github.com>
2020-04-09 03:22:14 +0100

    Create loops.cs

---
+++ 30DaysOfCode/loops.cs
@@ -0,0 +1,24 @@
+```csharp
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Solution 
+{
+    static void Main(string[] args) 
+    {
+        int n = Convert.ToInt32(Console.ReadLine());
+        Enumerable.Range(1, 10).ToList().ForEach(p => Console.WriteLine($"{n} x {p} = {n * p}"));
+    }
+}
+```
